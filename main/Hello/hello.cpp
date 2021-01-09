29b72acd946d4b5dc1999390dde0f352933de387
ycastroa <ycastroa@gmu.edu>
2021-01-09 11:56:52 -0500

    Day #2 C++

---
+++ main/Hello/hello.cpp
@@ -0,0 +1,21 @@
+/************************
+ * author Yamil
+ * 
+ * 01/09/2018 Yamil Castro
+ * 
+ * **********************/
+
+#include <iostream>
+
+//This is a comment
+/*this is a multiplle comment
+ 
+ */
+ 
+int main(){
+	int favorite_number;
+	std::cout<<"programming in c++ is fun"<<std::endl;
+	std::cin>>favorite_number;
+	std::cout<<"printing cin "<<favorite_number<<std::endl;
+	return 0;
+}
