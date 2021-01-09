9f980e7b827594753105ff580813a4fd5f108c89
ycastroa <ycastroa@gmu.edu>
2021-01-09 16:29:46 -0500

    day#2 class

---
+++ main/NameSpaces/day2.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+
+	int favorite_number;
+	cout<<"programming in c++ is fun"<<std::endl;
+	cin>>favorite_number;
+	cout<<"printing cin "<<favorite_number<<std::endl;
+	return 0;
+
+}	
+	
